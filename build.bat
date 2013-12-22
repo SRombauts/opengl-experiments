34f66fa592ce51cb6edbbbc602cbdb1ced94172c
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2013-12-22 18:06:03 +0100

    Fixed VS cpplint&cppcheck warning formatting
     + Doxygen description

--- build.bat
+++ build.bat
@@ -3,10 +3,10 @@ premake\premake4 vs2010
 @echo "[open and build solution]"
 
 @echo "==== Running cpplint ===="
-python cpplint.py --verbose=3 --output=eclipse --linelength=120 src/Main/Main.cpp src/Main/App.h src/Main/App.cpp src/Main/Input.h src/Main/Input.cpp src/Main/Renderer.h src/Main/Renderer.cpp src/Main/Scene.h src/Main/Scene.cpp src/Main/Node.h src/Main/Node.cpp src/Main/Mesh.h src/Main/Mesh.cpp
+python cpplint.py --verbose=3 --output=vs7 --linelength=120 src/Main/Main.cpp src/Main/App.h src/Main/App.cpp src/Main/Input.h src/Main/Input.cpp src/Main/Renderer.h src/Main/Renderer.cpp src/Main/Scene.h src/Main/Scene.cpp src/Main/Node.h src/Main/Node.cpp src/Main/Mesh.h src/Main/Mesh.cpp
 
 @echo "==== Running cppcheck ===="
-cppcheck --quiet --enable=style --template=gcc src/
+cppcheck --quiet --enable=style --template=vs src/
 
 @echo "==== Running doxygen ===="
 doxygen > NUL
